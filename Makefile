c89c2c363b506b39c2e901e444db40577f972d4d
tuxmux28 <ch_86@web.de>
2012-12-05 15:54:38 +0100

    Steuerung der Heizmatte hinzugefuegt

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 controlTemp: controlTemp.cpp tempSensor.o tempSensor.h
-	g++ -o controlTemp -Wall controlTemp.cpp tempSensor.o -L tempSensor.h
+	g++ -o controlTemp -Wall controlTemp.cpp tempSensor.o RCSwitch.o -L tempSensor.h -L RCSwitch.h -lrrd -l wiringPi
--- controlTemp.cpp
+++ controlTemp.cpp
@@ -11,6 +11,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <sstream>
+#include <vector>
 
 
 extern "C" {
@@ -18,62 +19,105 @@ extern "C" {
 }
 
 #include "tempSensor.h"
+#include "RCSwitch.h"
 
 using namespace std;
 
 
-int main (void) {
 
+void writeRrd (float temp1, float temp2, float temp3) {
 	int rrd_argc=3;
 	char *rrd_argv[rrd_argc];
-	float temp1, temp2, temp3;
-	int ret;
 	stringstream stream;
+//	rrd_argv[0]= new char[6];
+//	strcpy(rrd_argv[0],"update\0");
+	rrd_argv[0]="update";
+//	rrd_argv[1]= new char[36];
+//	strcpy(rrd_argv[1],"/home/pi/stuff/readTemp/tempGrow.rrd\0");
+	rrd_argv[1]="/home/pi/stuff/readTemp/tempGrow.rrd";
 
+	stream << "N:" << temp1 << ":" << temp2 << ":" << temp3 << ends;
+
+	rrd_argv[2] = new char[stream.str().size()];
+	strcpy(rrd_argv[2],stream.str().c_str());
+	
+
+ 	int ret = rrd_update(rrd_argc,rrd_argv);
+//	cout << ret << " " <<rrd_argv[0] << " " << rrd_argv[1] << " " <<  "String: " << rrd_argv[2] << endl;
+
+	stream.str("");
+	stream.clear();
+
+//	for ( int i = 0; i < rrd_argc; i++)
+//		delete[] rrd_argv[i];			
+
+		delete[] rrd_argv[2];
+}
+
+int main (void) {
+
+	wiringPiSetup();
+	RCSwitch heizmatte = RCSwitch();
+	heizmatte.enableTransmit(1);
+
+	float temp1, temp2, temp3;
+	vector<tempSensor> tempSensors;
 	tempSensor sens1("28-000003c31d58"), sens2("28-000004071f35"),
 							sens3("28-0000040743a0");
+
+	tempSensors.push_back(sens1);
+	tempSensors.push_back(sens2);
+	tempSensors.push_back(sens3);
+
 
 	while (1) {
 
 
-		rrd_argv[0]="update";
-		rrd_argv[1]="/home/pi/stuff/readTemp/tempGrow.rrd";
 
 		temp1 = sens1.getTemp();
 		temp2 = sens2.getTemp();
 		temp3 = sens3.getTemp();
 
+/*
 		if ( sens1.getLastTemp() != 0.0 && ( temp1 < sens1.getLastTemp() - 5 || temp1 > sens1.getLastTemp() + 5 )) { 
+			cout << "Before: Temp1: " << temp1 << " Last-Temp: " << sens1.getLastTemp() << endl;
 			temp1 = sens1.getLastTemp();
 			sens1.setLastTemp(temp1);
+			cout << "After:  Temp1: " << temp1 << " Last-Temp: " << sens1.getLastTemp() << endl;
 		} 
 		if ( sens2.getLastTemp() != 0.0 && ( temp2 < sens2.getLastTemp() - 5 || temp2 > sens2.getLastTemp() + 5 )) { 
+			cout << "Before: Temp2: " << temp2 << " Last-Temp: " << sens2.getLastTemp() << endl;
 			temp2 = sens2.getLastTemp();
 			sens2.setLastTemp(temp2);
+			cout << "After:  Temp2: " << temp1 << " Last-Temp: " << sens2.getLastTemp() << endl;
 		} 
 		if ( sens3.getLastTemp() != 0.0 && ( temp3 < sens3.getLastTemp() - 5 || temp3 > sens3.getLastTemp() + 5 )) { 
+			cout << "Before: Temp3: " << temp3 << " Last-Temp: " << sens3.getLastTemp() << endl;
 			temp3 = sens3.getLastTemp();
 			sens3.setLastTemp(temp3);
+			cout << "After:  Temp3: " << temp3 << " Last-Temp: " << sens3.getLastTemp() << endl;
 		} 
-
-		stream << "N:" << temp1 << ":" << temp2 << ":" << temp3 << ends;
-
-		rrd_argv[2] = new char[stream.str().size()];
-		
-		strcpy(rrd_argv[2],stream.str().c_str());
-
-		ret = rrd_update(rrd_argc,rrd_argv);
-
-/*		cout << "Temp1: " << temp1 << " Temp2: " << temp2
-			<< " Temp3: " << temp3 << " Ret: " << ret << " Stream: " << rrd_argv[2] <<  endl;
 */
 
-		stream.str("");
-		stream.clear();
 
-		free(rrd_argv[2]);			
+/*		cout << "Temp1: " << temp1 << " Temp2: " << temp2
+			<< " Temp3: " << temp3 << " Ret: " <<  endl;
+*/	
+/*		for (vector<tempSensor>::iterator it = tempSensors.begin(); it != tempSensors.end(); ++it ) {
+			cout << it->getTemp() << endl;
+		}		
+*/
 
-		sleep(60);
+	writeRrd(temp1, temp2, temp3);
+
+	if ( temp1 >= 28 ) 
+		heizmatte.switchOff("11111",3);
+	else if ( temp1 <= 26 ) 
+		heizmatte.switchOn("11111",3);
+		
+
+
+		sleep(1);
 	}
 
 }
